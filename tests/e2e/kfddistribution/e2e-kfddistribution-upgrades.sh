4ca784c4daed183a799dd506034ba0e9b32b8180
Manuel Romei <manuel.romei@reevo.it>
2025-04-16 16:34:07 +0200

    Re-enable e2e eks tests and add EKS force delete  (#389)
    * ci: reenable EKS and see what's going on
    
    * tests: use consistent nodepool size in all tests
    
    * chore: update furyctl version and add force delete
    
    * fix: create an output directory different from the distro-location
    
    * tests(e2e): use new furyctl rc version
    
    * tests: refine EKS tests trigger on drone
    
    * fix: actually reference an existing cron job
    
    Drone crons need to be referenced by name. Drone was not firing any
    cronjob because it was expecting a cron job named "0 23 * * *" lol.
    
    * chore: use cron to test
    
    * chore: add platform to aws nuke
    
    * fix: use cron
    
    * fix: use updated aws-nuke
    
    * chore: update deprecated components in AWS nuke
    
    * chore: add cron
    
    * ci(e2e): update paths and cleanup ekscluster tests (#390)
    
    * chore: organize e2e tests files
    
    * fix(e2e): remove duplicate migrations from ekscluster provider
    
    * fix: restore old cron
    
    ---------
    
    Co-authored-by: Stefano Ghinelli <stefano.ghinelli@sighup.io>

--- tests/e2e/kfddistribution/e2e-kfddistribution-upgrades.sh
+++ tests/e2e/kfddistribution/e2e-kfddistribution-upgrades.sh
@@ -7,8 +7,8 @@ set -e
 
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl for the initial setup 1.31.0"
-/tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.31.0.yaml --outdir "$PWD" --disable-analytics
+/tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/manifests/furyctl-init-cluster-1.31.0.yaml --outdir "$PWD" --disable-analytics
 
 echo "----------------------------------------------------------------------------"
 echo "Executing upgrade to 1.31.1"
-/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/furyctl-init-cluster-1.31.1.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/manifests/furyctl-init-cluster-1.31.1.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
