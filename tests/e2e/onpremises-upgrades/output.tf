5c176a56fd922c5c3b94a274d75a47be369f5000
Samuele Chiocca <samuele@nutellino.it>
2025-05-15 18:04:06 +0200

    Feat: add onpremises e2e upgrade tests (#410)
    * feat: WIP e2e-onpremises upgrades pipelines and tests
    
    * chore: remove kyverno test from upgrades since we are using gatekeeper
    
    * fix: drone pipeline duplicate step name
    
    * fix: drone upgrade pipeline, wrong directory on tofu infrastructure creation
    
    * fix: delete step on upgrade was using the wrong folder
    
    * feat: make standard onpremises e2e test to use current distribution repository for tests. Now the hardcoded distribution version in the tofu output is the target distribution version we want to release

--- tests/e2e/onpremises-upgrades/output.tf
+++ tests/e2e/onpremises-upgrades/output.tf
@@ -18,6 +18,156 @@ kind: OnPremises
 metadata:
   name: reevo
 spec:
+  distributionVersion: v1.31.1
+  kubernetes:
+    pkiFolder: ./pki
+    ssh:
+      username: root
+      keyPath: /cache/ci-ssh-key
+    dnsZone: nip.io
+    controlPlaneAddress: ${replace(hcloud_server.haproxy.ipv4_address, ".", "-")}.nip.io:6443
+    podCidr: 10.128.0.0/14
+    svcCidr: 172.30.0.0/16
+    loadBalancers:
+      enabled: true
+      hosts:
+        - name: haproxy-10-10-1-2
+          ip: 10.10.1.2
+      keepalived:
+        enabled: false
+        interface: enp0s6
+        ip: 10.250.250.179/32
+        virtualRouterId: "201"
+        passphrase: "b16cf069"
+      stats:
+        username: admin
+        password: "password"
+      additionalConfig: "{file://./haproxy-additional.cfg}"
+    masters:
+      hosts:
+        - name: controlplane0-10-10-1-3
+          ip: 10.10.1.3
+        - name: controlplane1-10-10-1-4
+          ip: 10.10.1.4
+        - name: controlplane2-10-10-1-5
+          ip: 10.10.1.5
+    nodes:
+      - name: infra
+        hosts:
+          - name: infra0-10-10-1-6
+            ip: 10.10.1.6
+          - name: infra1-10-10-1-7
+            ip: 10.10.1.7
+          - name: infra2-10-10-1-8
+            ip: 10.10.1.8
+        taints: []
+      - name: worker
+        hosts:
+          - name: worker0-10-10-1-9
+            ip: 10.10.1.9
+          - name: worker1-10-10-1-10
+            ip: 10.10.1.10
+        taints: []
+    #advancedAnsible:
+      #config: |
+      #  ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -q root@${hcloud_server.haproxy.ipv4_address}"'
+    advanced: 
+      encryption:
+        configuration: "{file://./encrypted-secret-config.yaml}"
+  distribution:
+    common:
+      nodeSelector:
+         node.kubernetes.io/role: infra
+      #tolerations:
+      #  - effect: NoSchedule
+      #    key: node.kubernetes.io/role
+      #    value: infra
+      networkPoliciesEnabled: true
+    modules:
+      networking:
+        type: calico
+      ingress:
+        baseDomain: ingress.${replace(hcloud_server.haproxy.ipv4_address, ".", "-")}.nip.io
+        nginx:
+          type: single
+          tls:
+            provider: secret
+            secret:
+              cert: "{file://tls.crt}"
+              key: "{file://tls.key}"
+              ca: "{file://ca.crt}"
+        certManager:
+          clusterIssuer:
+            name: letsencrypt-fury
+            email: samuele.chiocca@reevo.it
+            type: http01
+      logging: 
+        type: loki
+        opensearch:
+          type: triple
+        loki: 
+          backend: minio
+          tsdbStartDate: "2024-11-18"
+      monitoring:
+        type: mimir
+        mimir: 
+          retentionTime: 3d
+          backend: minio
+        prometheus:
+          retentionTime: 1d
+          retentionSize: 5GiB
+          storageSize: 20Gi
+        alertmanager:
+          installDefaultRules: false
+      policy: 
+        type: gatekeeper
+        gatekeeper:
+          enforcementAction: warn
+          installDefaultPolicies: true 
+      dr:
+        type: on-premises
+        velero: 
+          backend: minio
+          schedules:
+            install: true
+            definitions:
+              full:
+                snapshotMoveData: true
+          snapshotController:
+            install: true
+      tracing: 
+        type: tempo
+        tempo: 
+          backend: minio
+      auth:
+        provider:
+          type: none
+
+
+  plugins:
+    helm:
+      repositories:
+        - name: longhorn
+          url: https://charts.longhorn.io
+      releases:
+        - name: longhorn
+          namespace: longhorn-system
+          chart: longhorn/longhorn
+          version: '1.8.1'
+          set:
+            - name: persistence.defaultClassReplicaCount
+              value: "2"
+EOF
+}
+
+output "furyctl_upgrade_yaml" {
+  value = <<EOF
+---
+apiVersion: kfd.sighup.io/v1alpha2
+kind: OnPremises
+metadata:
+  name: reevo
+spec:
   distributionVersion: v1.32.0
   kubernetes:
     pkiFolder: ./pki
@@ -85,7 +235,7 @@ spec:
       networkPoliciesEnabled: true
     modules:
       networking:
-        type: cilium
+        type: calico
       ingress:
         baseDomain: ingress.${replace(hcloud_server.haproxy.ipv4_address, ".", "-")}.nip.io
         nginx:
@@ -120,11 +270,10 @@ spec:
         alertmanager:
           installDefaultRules: false
       policy: 
-        type: kyverno
-        kyverno:
-          additionalExcludedNamespaces: ["longhorn-system"]
-          installDefaultPolicies: true
-          validationFailureAction: Audit    
+        type: gatekeeper
+        gatekeeper:
+          enforcementAction: warn
+          installDefaultPolicies: true 
       dr:
         type: on-premises
         velero: 
@@ -157,10 +306,9 @@ spec:
           version: '1.8.1'
           set:
             - name: persistence.defaultClassReplicaCount
-              value: "1"
+              value: "2"
 EOF
 }
-
 
 output "req_dns" {
   value = <<EOF
