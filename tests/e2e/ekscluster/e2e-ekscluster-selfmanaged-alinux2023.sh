508eed3cb1eac1eec197d30db8f809686d95f757
Filo01 <git@filippo.sh>
2025-05-30 12:18:02 +0200

    fix(e2e): fix drone pipeline

--- tests/e2e/ekscluster/e2e-ekscluster-selfmanaged-alinux2023.sh
+++ tests/e2e/ekscluster/e2e-ekscluster-selfmanaged-alinux2023.sh
@@ -8,16 +8,6 @@ set -e
 LAST_FURYCTL_YAML=tests/e2e/ekscluster/manifests/furyctl-selfmanaged-alinux2-init-cluster.yaml
 tests/e2e/ekscluster/replace_variables.sh --distribution-version "$DISTRIBUTION_VERSION" --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$LAST_FURYCTL_YAML"
 echo "----------------------------------------------------------------------------"
-echo "Executing furyctl with self-managed nodes with alinux2"
-tests/e2e/ekscluster/furyctl_apply.expect "$LAST_FURYCTL_YAML" /tmp ./
-echo "$LAST_FURYCTL_YAML" > last_furyctl_yaml.txt
-echo "Testing that the components are running"
-bats -t tests/e2e/ekscluster/e2e-ekscluster-init-cluster.sh
-
-
-LAST_FURYCTL_YAML=tests/e2e/ekscluster/manifests/furyctl-selfmanaged-alinux2-init-cluster.yaml
-tests/e2e/ekscluster/replace_variables.sh --distribution-version "$DISTRIBUTION_VERSION" --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$LAST_FURYCTL_YAML"
-echo "----------------------------------------------------------------------------"
 echo "Executing furyctl with self-managed nodes with alinux2023"
 tests/e2e/ekscluster/furyctl_apply.expect "$LAST_FURYCTL_YAML" /tmp ./
 echo "$LAST_FURYCTL_YAML" > last_furyctl_yaml.txt
