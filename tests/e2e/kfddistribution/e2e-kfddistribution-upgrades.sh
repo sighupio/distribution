82f06d75071a0250efba3954db570bff0614e3da
Stefano Ghinelli <stefano.ghinelli@sighup.io>
2025-09-23 08:54:00 +0200

    test: add kfd 1.33 e2e

--- tests/e2e/kfddistribution/e2e-kfddistribution-upgrades.sh
+++ tests/e2e/kfddistribution/e2e-kfddistribution-upgrades.sh
@@ -6,9 +6,9 @@
 set -e
 
 echo "----------------------------------------------------------------------------"
-echo "Executing furyctl for the initial setup 1.31.1"
-/tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/manifests/furyctl-init-cluster-1.31.1.yaml --outdir "$PWD" --disable-analytics
+echo "Executing furyctl for the initial setup 1.32.0"
+/tmp/furyctl apply --config tests/e2e/kfddistribution-upgrades/manifests/furyctl-init-cluster-1.32.0.yaml --outdir "$PWD" --disable-analytics
 
 echo "----------------------------------------------------------------------------"
-echo "Executing upgrade to 1.32.0"
-/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/manifests/furyctl-init-cluster-1.32.0.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
+echo "Executing upgrade to 1.33.0"
+/tmp/furyctl apply --upgrade --config tests/e2e/kfddistribution-upgrades/manifests/furyctl-init-cluster-1.33.0.yaml --outdir "$PWD" --distro-location ./ --force upgrades --disable-analytics
