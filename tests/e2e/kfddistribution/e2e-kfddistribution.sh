697e51c8bf9ef8bab332b5cc5ef065690c3b5707
Stefano Ghinelli <stefano.ghinelli@sighup.io>
2025-09-25 17:35:40 +0200

    testing: e2e update for KFD provider (#446)
    * test: add kfd 1.33 e2e
    
    * test: comment patching the furyctl-init-cluster.yaml with secured registry
    
    * test: restore kfddistribution upgrade pipeline
    
    * test: restore patching with secured registry in kfd e2e tests

--- tests/e2e/kfddistribution/e2e-kfddistribution.sh
+++ tests/e2e/kfddistribution/e2e-kfddistribution.sh
@@ -16,7 +16,7 @@ echo "Executing furyctl cleanup all modules and configurations"
 /tmp/furyctl create cluster --config tests/e2e/kfddistribution/manifests/furyctl-cleanup-all.yaml --outdir "$PWD" --distro-location ./ --skip-deps-download --force all --disable-analytics
 bats -t tests/e2e/kfddistribution/e2e-kfddistribution-cleanup-all.sh
 
-echo "----------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------" 
 echo "Patching the furyctl-init-cluster.yaml to use the secured registry"
 yq e -i '.spec.distribution.common.registry = "registry.sighup.io/fury-secured"' tests/e2e/kfddistribution/manifests/furyctl-init-cluster.yaml
 
