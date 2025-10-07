d90c28e21368e21c55e0d0476a38086b793e9558
Manuel Romei <manuel.romei@reevo.it>
2025-10-07 10:28:16 +0200

    feat: backport v1.33.0 edits to v1.31.2

--- tests/e2e/ekscluster-upgrades/e2e-ekscluster-upgrades.sh
+++ tests/e2e/ekscluster-upgrades/e2e-ekscluster-upgrades.sh
@@ -6,13 +6,19 @@
 set -e
 
 echo "----------------------------------------------------------------------------"
-echo "Executing furyctl for the initial setup 1.30.1"
-FURYCTL_YAML=/drone/src/tests/e2e/ekscluster-upgrades/manifests/furyctl-init-cluster-1.30.1.yaml
+echo "Executing furyctl for the initial setup 1.32.0 with alinux2"
+FURYCTL_YAML=/drone/src/tests/e2e/ekscluster-upgrades/manifests/furyctl-init-cluster-1.32.0-alinux2.yaml
 /drone/src/tests/e2e/ekscluster/replace_variables.sh --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$FURYCTL_YAML"
 /drone/src/tests/e2e/ekscluster/furyctl_apply.expect $FURYCTL_YAML /tmp
 
 echo "----------------------------------------------------------------------------"
-echo "Executing upgrade to 1.31.1"
-FURYCTL_YAML=tests/e2e/ekscluster-upgrades/manifests/furyctl-init-cluster-1.31.1.yaml
+echo "Executing nodepool migration to alinux2023 (with 1.32.0)"
+FURYCTL_YAML=tests/e2e/ekscluster-upgrades/manifests/furyctl-migrate-nodepool-1.32.0-alinux2023.yaml
+tests/e2e/ekscluster/replace_variables.sh --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$FURYCTL_YAML"
+/drone/src/tests/e2e/ekscluster/furyctl_apply.expect $FURYCTL_YAML /tmp
+
+echo "----------------------------------------------------------------------------"
+echo "Executing version upgrade to 1.33.0 (with alinux2023)"
+FURYCTL_YAML=tests/e2e/ekscluster-upgrades/manifests/furyctl-upgrade-version-1.33.0.yaml
 tests/e2e/ekscluster/replace_variables.sh --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$FURYCTL_YAML"
 tests/e2e/ekscluster-upgrades/furyctl_upgrade.expect $FURYCTL_YAML /tmp ./
