9a36537708ed180f9c54e1d9a770b850e7cee8de
Manuel Romei <manuel.romei@reevo.it>
2025-10-08 16:02:37 +0200

    ci: fix eks upgrade

--- tests/e2e/ekscluster-upgrades/e2e-ekscluster-upgrades.sh
+++ tests/e2e/ekscluster-upgrades/e2e-ekscluster-upgrades.sh
@@ -15,4 +15,4 @@ echo "--------------------------------------------------------------------------
 echo "Executing furyctl for the upgrade to 1.31.2 with alinux2023"
 FURYCTL_YAML=tests/e2e/ekscluster-upgrades/manifests/furyctl-upgrade-version-1.31.2-alinux2023.yaml
 tests/e2e/ekscluster/replace_variables.sh --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$FURYCTL_YAML"
-/drone/src/tests/e2e/ekscluster/furyctl_apply.expect $FURYCTL_YAML /tmp
+tests/e2e/ekscluster-upgrades/furyctl_upgrade.expect $FURYCTL_YAML /tmp ./
