3d51cc6ae5ccc9ede52ad110dab610ac12e6a3ce
Samuele Chiocca <samuele@sighup.io>
2025-10-22 16:48:24 +0200

    feat: simplify eks pipeline, remove expect dependency and scripts

--- tests/e2e/ekscluster/e2e-ekscluster.sh
+++ tests/e2e/ekscluster/e2e-ekscluster.sh
@@ -9,7 +9,14 @@ LAST_FURYCTL_YAML=tests/e2e/ekscluster/manifests/furyctl-init-cluster.yaml
 tests/e2e/ekscluster/replace_variables.sh --distribution-version "$DISTRIBUTION_VERSION" --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$LAST_FURYCTL_YAML"
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl for the initial setup"
-tests/e2e/ekscluster/furyctl_apply.expect "$LAST_FURYCTL_YAML" /tmp ./
+furyctl apply \
+  --outdir /tmp \
+  --config "$LAST_FURYCTL_YAML" \
+  --disable-analytics \
+  --distro-location ./ \
+  --force all \
+  --skip-vpn-confirmation \
+  --no-tty
 echo "$LAST_FURYCTL_YAML" > last_furyctl_yaml.txt
 echo "Testing that the components are running"
 bats -t tests/e2e/ekscluster/e2e-ekscluster-init-cluster.sh
@@ -19,6 +26,13 @@ LAST_FURYCTL_YAML=tests/e2e/ekscluster/manifests/furyctl-cleanup-all.yaml
 tests/e2e/ekscluster/replace_variables.sh --distribution-version "$DISTRIBUTION_VERSION" --cluster-name "$CLUSTER_NAME" --furyctl-yaml "$LAST_FURYCTL_YAML"
 echo "----------------------------------------------------------------------------"
 echo "Executing furyctl cleanup all modules and configurations"
-tests/e2e/ekscluster/furyctl_apply.expect "$LAST_FURYCTL_YAML" /tmp ./
+furyctl apply \
+  --outdir /tmp \
+  --config "$LAST_FURYCTL_YAML" \
+  --disable-analytics \
+  --distro-location ./ \
+  --force all \
+  --skip-vpn-confirmation \
+  --no-tty
 echo "$LAST_FURYCTL_YAML" > last_furyctl_yaml.txt
 bats -t tests/e2e/ekscluster/e2e-ekscluster-cleanup-all.sh
