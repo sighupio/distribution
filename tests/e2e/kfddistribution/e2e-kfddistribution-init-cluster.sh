a3c6608ecab17b23ed6f98570b7d8db13e3460f4
Stefano Ghinelli <stefano.ghinelli@sighup.io>
2026-02-16 12:16:18 +0100

    testing: update kfddistro e2e tests

--- tests/e2e/kfddistribution/e2e-kfddistribution-init-cluster.sh
+++ tests/e2e/kfddistribution/e2e-kfddistribution-init-cluster.sh
@@ -68,6 +68,16 @@ load ./helper
     [ "$status" -eq 0 ]
 }
 
+@test "HAProxy Ingress Controller is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=haproxy-ingress -o json -n ingress-haproxy |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
 @test "Forecastle is Running" {
     info
     test() {
