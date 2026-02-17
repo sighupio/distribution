e5d39bd290d2383f5e043dae4e99d9bed1b92a8f
Stefano Ghinelli <stefano.ghinelli@sighup.io>
2026-02-17 15:22:32 +0100

    testing: update on-prem e2e tests

--- tests/e2e/onpremises-upgrades/e2e-onpremises.sh
+++ tests/e2e/onpremises-upgrades/e2e-onpremises.sh
@@ -47,6 +47,16 @@ load ./helper
     [ "$status" -eq 0 ]
 }
 
+@test "HAProxy Ingress Controller is Running" {
+    info
+    test() {
+        kubectl get pods -l app.kubernetes.io/name=kubernetes-ingress -o json -n ingress-haproxy |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 10
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
 @test "Forecastle is Running" {
     info
     test() {
